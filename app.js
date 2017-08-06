87a80dc7486178464adf3f95c8a06242e5c94698
Sergey <vor.ser87@gmail.com>
2017-08-06 22:49:19 +0300

    create application uses promise

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var express = require('express'),
     pg = require('pg'),
     //Pool = require('pg-pool'),
     app = express();
+    dbConfig = require('./config/db');
+    db = require('./db_connection');
+    session = require('express-session')
 
-
-//DB connection
-var connect = "postgres://eduonix:123456@localhost/recipebookdb";
 
 //Assign Dust Engine to .dust Files
 app.engine('dust',cons.dust);
@@ -22,23 +22,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.get('/', function(req, res){
-    pg.connect(connect, function(err, client, done){
-        if(err){
-            return console.error('err fetching client from pool', err);
-        }
-        client.query('SELECT * FROM recipes', function(err, result){
-            if(err){
-                return console.error('error running query', err)
-            }
+//var db = pgp('postgres://eduonix:123456@localhost:5432/recipebookdb');
+/*console.log('======>', db);*/
 
-            res.render('index', {recipes: result.rows});
-            done();
-        });
+app.get('/', function(req, res){
+
+    console.log('======>', db);
+    //Set deg Ext .dust
+    db.any('SELECT * FROM recipes', [true])
+    .then(function(data){
+        res.render('index', {recipes: data});
+        console.log(data);
+    })
+    .catch(function(err){
+        console.log('Ошибка----->', err);
     });
+    /*pg.connect(connect, function(err, client, done){
+       if(err){
+           return console.error('err fetching client from pool', err);
+       }
+       client.query('SELECT * FROM recipes', function(err, result){
+           if(err){
+               return console.error('error running query', err)
+           }
+
+           res.render('index', {recipes: result.rows});
+           done();
+       });
+   });*/
 });
 app.post('/add', function(req, res){
-    pg.connect(connect, function(err, client, done){
+    /*pg.connect(connect, function(err, client, done){
         if(err){
             return console.error('err fetching client from pool', err);
         }
@@ -47,10 +61,22 @@ app.post('/add', function(req, res){
             res.redirect('/');
 
 
+    });*/
+    db.none('INSERT INTO recipes (name,ingredients,direction) VALUES ($1,$2,$3)',
+        [req.body.name,
+         req.body.ingredients,
+         req.body.direction]
+    )
+    .then(() => {
+            console.log('Успешно додана запись!')
+        res.redirect('/');
+    })
+    .catch(error => {
+        console.log(error);
     });
 });
 app.delete('/delete/:id', function(req, res){
-    pg.connect(connect, function(err, client, done){
+   /* pg.connect(connect, function(err, client, done){
         if(err){
             return console.error('err fetching client from pool', err);
         }
@@ -59,10 +85,18 @@ app.delete('/delete/:id', function(req, res){
         res.sendStatus(200);
 
 
+    });*/
+    db.any('DELETE FROM recipes WHERE id=$1',[req.params.id])
+    .then(() => {
+            console.log('Запись успешно удалена');
+            res.sendStatus(200);
+    })
+    .catch(error => {
+        console.log(error);
     });
 });
 app.post('/edit', function(req, res){
-    pg.connect(connect, function(err, client, done){
+    /*pg.connect(connect, function(err, client, done){
         if(err){
             return console.error('err fetching client from pool', err);
         }
@@ -71,9 +105,17 @@ app.post('/edit', function(req, res){
         res.redirect('/');
 
 
-    });
+    });*/
+    db.any('UPDATE recipes SET name=$1, ingredients=$2, direction=$3  WHERE id=$4',[req.body.name, req.body.ingredients, req.body.direction, req.body.id])
+    .then(()=>{
+           console.log('Запись успешно изменена');
+            res.redirect('/');
+    })
+    .catch(error => {
+        console.log('Ошибка при редактировании =====>', error);
+    })
 });
 //Server
-app.listen(3000, function(req, res){
+app.listen(dbConfig.server_port, function(req, res){
     console.log('Server Started On Port 3000');
 });
---
+++ config/db/index.js
@@ -0,0 +1,9 @@
+var config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'recipebookdb',
+    user: "eduonix",
+    password: '123456',
+    server_port: 3000
+};
+module.exports = config;
